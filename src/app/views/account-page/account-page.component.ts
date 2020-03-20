61efcc67f3bcda10ad3974342803326d71de40dc
Carissa <carissa.meg@gmail.com>
2020-03-20 16:11:03 -0500

    Router constructor

--- src/app/views/account-page/account-page.component.ts
+++ src/app/views/account-page/account-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router:Router
+  ) { }
 
   ngOnInit(): void {
   }

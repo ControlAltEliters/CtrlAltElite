3fce1755455aabb83191211d3734b10dd273fc5c
Derek <derekgrogers1@gmail.com>
2020-03-26 01:28:55 -0500

    Created chat box component

---
+++ src/app/views/partials/chat-box/chat-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-box',
+  templateUrl: './chat-box.component.html',
+  styleUrls: ['./chat-box.component.css']
+})
+export class ChatBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

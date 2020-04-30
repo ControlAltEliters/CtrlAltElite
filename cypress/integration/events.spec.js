0b29ff5b404b9594ecbfffd33cb02f09eadf37cb
Carissa <carissa.meg@gmail.com>
2020-04-30 00:15:34 -0500

    Add cypress automated integration testing

---
+++ cypress/integration/events.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context('Event Page', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:4200/events')
+    // cy.visit('http://45.76.237.107:4200/events')
+  })
+
+  it('should display calendar', () => {
+    cy.get('#full-calendar').should('exist')
+  })
+})
---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+context('Home Page', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:4200/')
+    // cy.visit('http://45.76.237.107:4200/')
+  })
+
+  it('should display event modal', () => {
+    cy.get('.sidebar-event').first().click();
+    cy.get('#eventModal').should('have.class', 'visible')
+  })
+
+  it('should display google map', () => {
+    cy.get('.map-row').should('be.visible')
+  })
+
+  it('should redirect to login when guest tries to join event', () => {
+    cy.get('.sidebar-event').first().click();
+    cy.get('#eventModal .actions div .button').click()
+
+    cy.url().should('include', '/login')
+  })
+
+  it('should successfully authenticate user', () => {
+    // cy.visit('http://45.76.237.107:4200/login')
+    cy.visit('http://127.0.0.1:4200/login')
+    cy.get('input[name=email]').type('testuser@yoohoo.com')
+    cy.get('input[name=password]').type('command')
+    cy.get('#login').click();
+
+    cy.url().should('include', '/home')
+    cy.get('.avatar').should('be.visible')
+
+    cy.get('#nav-logout').click();
+  })
+
+
+})
